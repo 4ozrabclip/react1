a4619a4c16457b8cc62854ee96f097748c88e927
4ozRabclip <109936932+4ozrabclip@users.noreply.github.com>
2023-11-08 13:17:42 +1300

    refactor
    deleted unnecesary components files from tutorial, rewrote it simpler inside app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import './App.css';
-import { TodoWrapper } from './components/TodoWrapper.js';
+import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTodo = () => {
+    if (task.trim() === '') return;
+    setTodos([...todos, task]);
+    setTask('');
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div className="App">
-      <TodoWrapper />
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
