9e11b329c743531c0b0d8472964ed9e5e876077f
4ozrabclip <4oz.developer@gmail.com>
2023-11-15 09:35:06 +1300

    update draggable for better touchscreen functional

--- src/Draggable.js
+++ src/Draggable.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import {useDraggable} from '@dnd-kit/core';
+import { useDraggable } from '@dnd-kit/core';
 
 export function Draggable(props) {
-  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
     id: 'draggable',
   });
-  const style = transform ? {
-    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-  } : undefined;
 
-  
+  // Add passive: true to the touch event listeners
+  const touchListeners = {
+    onTouchStart: listeners.onTouchStart,
+    onTouchMove: listeners.onTouchMove,
+    onTouchEnd: listeners.onTouchEnd,
+    onTouchCancel: listeners.onTouchCancel,
+  };
+
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
+
   return (
-    <label ref={setNodeRef} style={style} {...listeners} {...attributes}>
+    <label
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...touchListeners}
+      onTouchStart={(e) => e.preventDefault()} // Prevent default to disable touch scrolling during drag
+    >
       {props.children}
     </label>
   );
