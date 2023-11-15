8f3f723f9a02d2353438cac322409fa402360ba1
4ozrabclip <4oz.developer@gmail.com>
2023-11-15 18:20:15 +1300

    sortable 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
-import { DndContext } from '@dnd-kit/core';
+import { DndContext, closestCenter} from '@dnd-kit/core';
+import { SortableContext, verticalListSortingStrategy, useSortable} from '@dnd-kit/sortable';
+import { Droppable } from './Droppable.js';
 import { Draggable } from './Draggable.js';
 
 function App() {
@@ -25,11 +27,8 @@ function App() {
     setTodos(updatedTodos);
   };
 
-  const handleDrop = (event) => {
-    console.log("yep");
-  };
-
   return (
+    <DndContext collisionDetection={closestCenter}>
     <div className="App">
       <h1>Todo List</h1>
       <div>
@@ -46,32 +45,47 @@ function App() {
         />
         <button className="add-button" onClick={addTodo}>Add</button>
       </div>
-      <DndContext onDragEnd={handleDrop}>
-        <ul>
-          {todos.map((todo, index) => (
-              <li>
-                <div className="todo-container">
-                    <input
-                      type="checkbox"
-                      onChange={() => handleCheckboxChange(index)}
-                      checked={todo.completed}
-                      id={`todo-${index}`}
-                    />
-                    <Draggable key={todo}>
-                      <label htmlFor={`todo-${index}`} className={`todo-label ${todo.completed ? 'completed' : ''}`}>
+      <SortableContext items={todos} strategy={verticalListSortingStrategy}>
+          <ul>
+            {todos.map((todo, index) => (
+              <useSortable key={index} id={index}>
+                <Droppable>
+                  <li>
+                    <div className="todo-container">
+                      <input
+                        type="checkbox"
+                        onChange={() => handleCheckboxChange(index)}
+                        checked={todo.completed}
+                        id={`todo-${index}`}
+                      />
+                      <Draggable>
+                      <label
+                        htmlFor={`todo-${index}`}
+                        className={`todo-label ${
+                          todo.completed ? 'completed' : ''}`}>
                         {todo.task}
                       </label>
-                    </Draggable>
-                    <button className="remove-button" onClick={() => removeTodo(index)}>Remove</button>
-                </div>
-              </li>
-          ))}
-        </ul>
+                      </Draggable>
+                      <button
+                        className="remove-button"
+                        onClick={() => removeTodo(index)}>
+                        Remove
+                      </button>
+                    </div>
+                  </li>
+                </Droppable>
+              </useSortable>
+            ))}
+          </ul>
+        </SortableContext>
+        <Droppable>
         <div className="extra-container">
           <p>..</p>
               </div>
+              </Droppable>
+              </div>
       </DndContext>
-    </div>
+
   );
 }
 
